e1f860d3274b928db69fbe52860cef6e1bab21e7
L&LMsin <thelastselatrop@gmail.com>
2022-08-29 12:10:57 -0400

    C2 prueba3

--- src/js/teser.js
+++ src/js/teser.js
@@ -1,4 +1,4 @@
-const application = document.querySelector('#container1')
+const application = document.querySelector('#tarjetas1')
 
  /* http://168.138.133.24:4000/api/clientes  */
 
@@ -8,25 +8,64 @@ fetch(url)
 //.then(res => console.log(res))
 .then(res => res.json())
 .then(data => {
+    console.log(data)
     data.forEach(usuario => {
-        console.log(usuario.NOMBRE)
+        /*
         const p = document.createElement('p')
-        p.setAttribute('id', usuario.CORREO)
-        p.innerHTML = usuario.NOMBRE
-        p.addEventListener('click', function(){
-            const Gen0 = usuario.CORREO
-        })
-        //p.innerHTML = usuario.CORREO
+        p.innerHTML = usuario.ID + ' | ' +
+        usuario.ID_ROL + ' | ' + 
+        usuario.NOMBRE + ' | ' + 
+        usuario.CORREO + ' | ' + 
+        usuario.CONTRASENA + ' | ' + 
+        usuario.PAIS + ' | ' + 
+        usuario.CIUDAD + ' | ' + 
+        usuario.DIRECCION + ' | ' + 
+        usuario.TIPO_CLIENTE ;
         application.appendChild(p)
+        */
+
+        // Estructura
+        const GlobalCard = document.createElement('div');
+        GlobalCard.classList.add('card');
+        // Body
+        const BodyCard = document.createElement('div');
+        BodyCard.classList.add('card-body');
+        // Titulo
+        const TitleCard = document.createElement('h5');
+        TitleCard.classList.add('card-title');
+        TitleCard.textContent = usuario.NOMBRE;
+        // SubTitulo
+        const SubTCard = document.createElement('h6');
+        SubTCard.classList.add('card-subtitle', 'mb-2', 'text-muted');
+        SubTCard.textContent = usuario.TIPO_CLIENTE;
+        // Email
+        const EmailCard = document.createElement('p');
+        EmailCard.classList.add('card-text');
+        EmailCard.textContent = "<b>correo:</b> " + usuario.CORREO;
+        // IDs
+        const IDsCard = document.createElement('p');
+        IDsCard.classList.add('card-text');
+        IDsCard.textContent = "<b>correo:</b> " + usuario.ID + ' | ' + usuario.ID_ROL ;
+        // Ubicacion
+        const GeoCard = document.createElement('p');
+        GeoCard.classList.add('card-text');
+        GeoCard.textContent = "<b>correo:</b> " + usuario.PAIS + ', ' + usuario.CIUDAD + ', ' + usuario.DIRECCION;
+
+        const miNodoBoton = document.createElement('button');
+        miNodoBoton.classList.add('btn', 'btn-primary');
+        miNodoBoton.textContent = '+';
+        miNodoBoton.setAttribute('marcador', info.id);
+        miNodoBoton.addEventListener('click', anyadirProductoAlCarrito);*/
+        // Insertamos
+        BodyCard.appendChild(TitleCard);
+        BodyCard.appendChild(SubTCard);
+        BodyCard.appendChild(EmailCard);
+        BodyCard.appendChild(IDsCard);
+        BodyCard.appendChild(GeoCard);
+        GlobalCard.appendChild(BodyCard);
+        application.appendChild(GlobalCard);
+
     });
     //console.log(data)
 })
 .catch(err => console.log(err))
-
-
-
-
-// selector de...  ID???
-
-const application2 = document.querySelector('#container2')
-fetch(url +"/perfil/" + )
